4fe9ad4025b24c2bdedcd2819591c59857d6c9cc
acurzon <acurzon@serif.com>
2023-07-25 00:56:41 +0100

    feat: update serverless express

--- index.js
+++ index.js
@@ -1,24 +1,4 @@
-// App
-const express = require('express');
-const app = express(); 
-
-const routes = express.Router();
-
-app.get("/", (req, res) => {
-  res.send("Home Page");
-});
-
-app.get("/test", (req, res) => {
-  res.send("Test Page");
-});
-
-app.use(routes);
-
 // Lambda Handler
-const awsServerlessExpress = require('aws-serverless-express');
-
-const server = awsServerlessExpress.createServer(app);
-
-exports.handler = (event, content) => {
-    return awsServerlessExpress.proxy(server, event, content);
-}
+const serverlessExpress = require('@vendia/serverless-express')
+const app = require('./app')
+exports.handler = serverlessExpress({ app })
