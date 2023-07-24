9a00aaacee16f6d8a872d90e1097ceebb5b5fafa
acurzon <acurzon@serif.com>
2023-07-25 00:15:57 +0100

    feat: emoji fix

--- index.mjs
+++ index.mjs
@@ -13,7 +13,9 @@ export const handler = async (event) => {
     body: 
     `<html>
     <head>
+      <meta charset="UTF-8">
       <title>Lambda Test</title>
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
     </head>
       <style>
         h1 {
